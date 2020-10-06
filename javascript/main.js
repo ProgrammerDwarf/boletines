3ef229468c1b05337f9ffff0825d3e12465bb134
Reinid Valarino <reinidval@gmail.com>
2020-10-06 01:15:22 -0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -18,7 +18,7 @@ links.forEach(function(link) {
         content.classList.add("animate__slideOutRight");
         
         setTimeout(function() {
-            location.href = "/";
+            location.href = "/boletines";
         },600);
     });
 });
